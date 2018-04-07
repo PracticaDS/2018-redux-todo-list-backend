d04fad97ef30920f29f971d6b41cf574d2cf4d90
Javier Fernandes <javier.fernandes@gmail.com>
2018-04-07 12:40:04 -0300

    primer version retornando una lista de todos hardcodeada

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello')
+import express from 'express'
+
+const app = express()
+
+const items = [
+  { id: 0, text: 'Comprar verduras', done: false },
+  { id: 1, text: 'Sacar turno con el doctor', done: false },
+  { id: 2, text: 'Actualizar partida de nacimiento', done: false },
+]
+
+app.get('/todos', (req, res) => res.send(items))
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
