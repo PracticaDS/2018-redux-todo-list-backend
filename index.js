a951c963c3b25a452c87a7d34168230b66fd416d
Javier Fernandes <javier.fernandes@gmail.com>
2018-04-22 18:01:10 -0300

    Separando las rutas a otro archivo

--- index.js
+++ index.js
@@ -1,55 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import uuid from 'uuid/v4'
-import { update } from 'ramda'
+import todos from './src/routes/todos'
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 //
-
-const initialState = [
-  { id: uuid(), text: 'Comprar verduras', done: false },
-  { id: uuid(), text: 'Sacar turno con el doctor', done: false },
-  { id: uuid(), text: 'Actualizar partida de nacimiento', done: false },
-]
-
-let items = initialState
-
-const ok = { status: 'ok' }
-
-app.get('/todos', (req, res) => res.send(items))
-app.post('/reset', (req, res) => {
-  items = initialState
-  res.send(ok)
-})
-
-app.put('/todos/:id', (req, res) => {
-  const { id } = req.params
-  const item = req.body
-  items = update(items.findIndex(_ => _.id === id), item, items)
-  res.send(ok)
-})
-
-app.delete('/todos/:id', (req, res) => {
-  const { id } = req.params
-  items = items.filter(i => i.id !== id)
-  res.send(ok)
-})
-
-app.post('/todos', (req, res) => {
-  const item = req.body
-  const updatedItem = {
-    id: uuid(),
-    ...item,
-  }
-  items = items.concat(updatedItem)
-  res.send({
-    ...ok,
-    data: updatedItem,
-  })
-})
+app.use(todos)
 
 const port = 3001
 app.listen(port, () => {
