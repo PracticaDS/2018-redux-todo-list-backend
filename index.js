c7826dd73bb10acc9ef978bfa4c79d0a9614bd00
Javier Fernandes <javier.fernandes@gmail.com>
2018-04-22 23:20:55 -0300

    Primer implementación de websockets para eventos: update / delete

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import * as WebSocket from 'ws'
 import todos from './src/routes/todos'
 
+// express
+
 const app = express()
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+
+const server = require('http').createServer(app)
+
+const wss = new WebSocket.Server({ server })
+const broadcastEvent = e => {
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(e))
+    }
+  })
+}
 
 //
-app.use(todos)
-
-const port = 3001
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
-})
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(todos(broadcastEvent))
 
 app.use((err, req, res) => {
   const { message, stack } = err
   res.status(500).send({ status: 'error', message, stack })
+})
+
+// websockets
+
+wss.on('connection', client => {
+  console.log('connection stablished')
+  client.on('event', data => { console.log('GOT', data) });
+  client.on('disconnect', () => { console.log('disconnected') });
+})
+
+
+const port = 3001
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
 })
--- src/routes/todos.js
+++ src/routes/todos.js
@@ -2,48 +2,66 @@ import uuid from 'uuid/v4'
 import { update } from 'ramda'
 import { Router } from 'express'
 
-const router = Router()
+export default broadCastEvent => {
+  const router = Router()
 
-const initialState = [
-  { id: uuid(), text: 'Comprar verduras', done: false },
-  { id: uuid(), text: 'Sacar turno con el doctor', done: false },
-  { id: uuid(), text: 'Actualizar partida de nacimiento', done: false },
-]
+  const initialState = [
+    { id: uuid(), text: 'Comprar verduras', done: false },
+    { id: uuid(), text: 'Sacar turno con el doctor', done: false },
+    { id: uuid(), text: 'Actualizar partida de nacimiento', done: false },
+  ]
 
-let items = initialState
+  let items = initialState
 
-const ok = { status: 'ok' }
+  const ok = { status: 'ok' }
 
-router.get('/todos', (req, res) => res.send(items))
-router.post('/reset', (req, res) => {
-  items = initialState
-  res.send(ok)
-})
-
-router.put('/todos/:id', (req, res) => {
-  const { id } = req.params
-  const item = req.body
-  items = update(items.findIndex(_ => _.id === id), item, items)
-  res.send(ok)
-})
-
-router.delete('/todos/:id', (req, res) => {
-  const { id } = req.params
-  items = items.filter(i => i.id !== id)
-  res.send(ok)
-})
-
-router.post('/todos', (req, res) => {
-  const item = req.body
-  const updatedItem = {
-    id: uuid(),
-    ...item,
-  }
-  items = items.concat(updatedItem)
-  res.send({
-    ...ok,
-    data: updatedItem,
+  router.get('/todos', (req, res) => res.send(items))
+  router.post('/reset', (req, res) => {
+    items = initialState
+    res.send(ok)
   })
-})
 
-export default router
+  router.put('/todos/:id', (req, res) => {
+    const { id } = req.params
+    const item = req.body
+    items = update(items.findIndex(_ => _.id === id), item, items)
+
+    broadCastEvent({
+      type: 'UPDATED',
+      id,
+      item,
+      from: req.get('UserId'),
+    })
+
+    res.send(ok)
+  })
+
+  router.delete('/todos/:id', (req, res) => {
+    const { id } = req.params
+    items = items.filter(i => i.id !== id)
+
+    broadCastEvent({
+      type: 'DELETED',
+      id,
+      from: req.get('UserId'),
+    })
+
+    res.send(ok)
+  })
+
+  router.post('/todos', (req, res) => {
+    const item = req.body
+    const updatedItem = {
+      id: uuid(),
+      ...item,
+    }
+    items = items.concat(updatedItem)
+    res.send({
+      ...ok,
+      data: updatedItem,
+    })
+  })
+
+  return router
+
+}
