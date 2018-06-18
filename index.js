babde297f3be107939b9e8d00b2cdc1e5a49d9ef
Javier Fernandes <javier.fernandes@gmail.com>
2018-06-18 11:00:09 -0300

    Setting up travis to deploy images to heroku. Using

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 import start from './src/server'
 
-const port = 3001
+const port = process.env.PORT || 3001
 
 start().listen(port, () => {
   console.log(`TODO list backend listening on port ${port}!`)
